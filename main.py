a794cefd54336e6367f734ca87154044a991fd9f
Krondini <koro5940@colorado.edu>
2021-04-11 19:33:56 -0600

    Data uploaded

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import numpy as np 
 import pandas as pd 
 import matplotlib.pyplot as plt
+import SVD
 
 def main():
-    pass
+    df = pd.read_csv("data/steam.csv")
+    df = SVD.runSVD(df) #TODO: Implement Singular Value Decomposition to reduce cols in DataFrame
 
 if __name__ == '__main__':
     main()
