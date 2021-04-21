2c25e9fef3bbfab08bcdcad430fedb13fc83fc66
Konlan Rondini <35783041+Krondini@users.noreply.github.com>
2021-04-21 14:00:16 -0600

    Edited getUsers.py
    getUsers.py now returns a usre's name and a list of their owned games

--- getUsers.py
+++ getUsers.py
@@ -10,12 +10,30 @@ NOTE: We're using a static data set (steam.csv) along with a dynamically
     obtained set of users. These two lists will undoubtedly have dimension mismatches.
     We will correct that in main.py
 '''
+def getUserSummary(steamid: str):
+
+    key = '67651BAD4657CA147D02F2C879D4B287'
+    userSummary = requests.get("http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key="+key+"&steamids="+steamid).json()
+    userName = userSummary['response']['players'][0]['personaname']
+
+    # Collect all games ever played by user
+    # Then sort by most play time, forever
+    ownedGames = requests.get("http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key="+key+"&steamid="+steamid+"&format=json").json()['response']['games']
+    ownedGames = sorted(ownedGames, key= lambda game: game["playtime_forever"], reverse=True)
+    
+    return (userName, ownedGames)
+
+
 def getUserInfo(): 
     
-    key = '67651BAD4657CA147D02F2C879D4B287'
-    cats_steam_id = "76561198272988632"
+    key = '67651BAD4657CA147D02F2C879D4B287' #API key
+    cats_steam_id = "76561198272988632" #Cat's Steam ID, used as a starting point for useer gathering
     entry_link = "http://api.steampowered.com/ISteamUser/"
+    
     friendslist = requests.get(entry_link+"GetFriendList/v0001/?key="+key+"&steamid="+cats_steam_id+"&relationship=friend")
-    # print(len(friendslist.json()['friendslist']["friends"]))
     my_summary = requests.get(entry_link+"GetPlayerSummaries/v0002/?key="+key+"&steamids="+cats_steam_id)
-    print(my_summary.json())
+    
+    friend1 = friendslist.json()['friendslist']['friends'][0]
+    friend1_id = friend1['steamid']
+    userName, userGames = getUserSummary(friend1_id)
+    return (userName, userGames)
--- main.py
+++ main.py
@@ -13,10 +13,24 @@ import SVD
 import getUsers
 import findRecs
 
+def formRow(username: str, games: list, df: pd.DataFrame) -> pd.Series:
+
+    row = pd.Series(np.zeros(len(df))) #Initialize row of playtime to be all zero
+
+    for game in games: #Iterate over user's games
+        game_id = game['appid']
+        df_index = df.index[df['appid'] == game_id]
+        if len(df_index) == 0: #Skip games with no info in dataset
+            continue
+        df_index = df_index[0] #Only one number in list at this point, just want it not the whole list
+        row[df_index] = game['playtime_forever']
+
+    return row
+
 def main():
-    df = pd.read_csv("data/steam.csv")
-    game_names = df['name'].to_numpy()
-    print("Head of data set:\n{}\n\n".format(df.head()))
+    game_df = pd.read_csv("data/steam.csv")
+    game_ids = game_df['appid'].to_numpy()
+    print("Head of data set:\n{}\n\n".format(game_df.head()))
     reduced_df = None
     # reduced_df = SVD.runSVD(df) #TODO: Implement Singular Value Decomposition to reduce cols in DataFrame
     if not reduced_df: #To be used while we work on the SVD implementation
@@ -25,8 +39,20 @@ def main():
     else: #To be used once the SVD implementation is finished
         pass
 
-    getUsers.getUserInfo()
-
+    userName, userGames = getUsers.getUserInfo()
+    game1 = userGames[0]
+    print(game1)
+    user_df = pd.DataFrame([], columns=["User"]+list(game_ids))
+    
+    new_row = formRow(userName, userGames, game_df)
+    minutes_total = np.sum(new_row)
+    hours_total = minutes_total/60
+    days_total = hours_total/24
+    years_total = days_total/365
+    print("User: \'{}\' has a total of: {} minutes of gaming on steam.".format(userName, minutes_total))
+    print("This is equivalent to {} total hours of gaming.".format(hours_total))
+    print("{:.2f} days of playing.".format(days_total))
+    print("Or even {:.4f} years of playing... nerrrrrd!".format(years_total))
 
 if __name__ == '__main__':
     main()
